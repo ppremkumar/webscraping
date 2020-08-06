d4c45464e4b00432af51d32f2b279cfa5d00d582
Premlal Premkumar <premlal.premkumar@gmail.com>
2020-08-06 15:47:45 +0530

    Add files via upload

---
+++ cbbc.py
@@ -0,0 +1,49 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
+
+with webdriver.Chrome() as driver:
+    driver.maximize_window()
+    driver.get("https://www.bbc.co.uk/schedules/p00fzl9r")
+    time.sleep(5)
+    
+    a = driver.find_elements(By.XPATH, "//ol[@class='list-unstyled g-c-l']")
+    start_time_list = []
+    tvg_title_list = []
+    item1_list = []
+    item2_list = []
+    episode_number = []
+    for x in a:
+        b = x.find_elements(By.XPATH, "//ol[@class='list-unstyled g-c-l']/li")
+        for y in b:
+            start_time = y.find_elements(By.XPATH, "ol[@class='highlight-box-wrapper']/li//span[@class='timezone--time']")
+            for each_start_time in start_time:
+                start_time_list.append(each_start_time.text)
+            tvg_title = y.find_elements(By.XPATH, "ol[@class='highlight-box-wrapper']//h4[@class='programme__titles']//span[@class='programme__title delta']/span")
+            for each_tvg_title in tvg_title:
+                tvg_title_list.append(each_tvg_title.text)
+            programme_subtitle_centi = y.find_elements(By.XPATH, "ol[@class='highlight-box-wrapper']//h4[@class='programme__titles']//span[@class='programme__subtitle centi']")
+            for each_item in programme_subtitle_centi:
+                item1 = each_item.find_element(By.XPATH, 'span[1]')
+                item1_list.append(item1.text)
+                try:
+                    item2 = each_item.find_element(By.XPATH, 'span[2]')
+                    item2_list.append(item2.text)
+                except NoSuchElementException:
+                    item2_list.append("")
+            synopsis_others = y.find_elements(By.XPATH, "ol[@class='highlight-box-wrapper']//p[@class='programme__synopsis text--subtle centi']")
+            for each_item_synopsis_others in synopsis_others:
+                try:
+                    episode_number_item = each_item_synopsis_others.find_element(By.XPATH, 'abbr/span[1]')
+                    episode_number.append(episode_number_item.text)
+                except NoSuchElementException:
+                    episode_number.append("")
+
+    # print(len(start_time_list), start_time_list)
+    # print(len(tvg_title_list), tvg_title_list)
+    # print(len(item1_list), item1_list)
+    # print(len(item2_list), item2_list)
+    print(len(episode_number), episode_number)
